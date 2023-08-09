b1f980e5ad222d1747b65d6ab3f0ea7bfda21cf0
Joaquin Bourdet <50186449+Quilotoa@users.noreply.github.com>
2023-08-09 18:46:48 -0300

    Primer modificacion
    En esta primera modificacion, coloque un Sout con una pequeña presentación en el main

--- src/tramo3_guia1_ejerciciogrupal/Tramo3_guia1_ejerciciogrupal.java
+++ src/tramo3_guia1_ejerciciogrupal/Tramo3_guia1_ejerciciogrupal.java
@@ -8,6 +8,9 @@ public class Tramo3_guia1_ejerciciogrupal {
     
     public static void main(String[] args) {
         
+        System.out.println("Esta es una prueba de ejercicio grupal, yo soy Joaquin. A medida que se conecten");
+        System.out.println("Coloquen un Sout con la misma informacion para comprobar");
+        
     }
 
 }
